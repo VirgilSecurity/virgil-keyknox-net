b4b7ce0b039c1355526ca5c0f8e38406b9b9bfc8
Vasilina <marfachaiko@gmail.com>
2019-03-20 16:45:42 +0500

    Cloud key storage

---
+++ Keyknox/CloudKeyStorage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Keyknox.Utils;
+using Virgil.CryptoAPI;
+using Virgil.SDK.Common;
+using Virgil.SDK.Web.Authorization;
+
+namespace Keyknox
+{
+    public class CloudKeyStorage : IKeyStorage
+    {
+        private KeyknoxManager keyknoxManager;
+        private Utils.IJsonSerializer serializer;
+       // private Lazy<Task<Dictionary<string, CloudEntry>>> cloudEntries;
+        private Dictionary<string, CloudEntry> cloudEntries;
+        private byte[] keyknoxHash;
+        private bool syncWasCalled;
+        public CloudKeyStorage(KeyknoxManager keyknoxManager, Utils.IJsonSerializer serializer = null)
+        {
+            this.keyknoxManager = keyknoxManager;
+            this.serializer = serializer ?? new Utils.NewtonsoftJsonSerializer();
+           // this.cloudEntries = new Lazy<Task<Dictionary<string, CloudEntry>>>(
+            //    () => RetrieveCloudEntries()
+            //);
+
+        }
+
+        public CloudKeyStorage(IAccessTokenProvider accessTokenProvider,
+                              IPrivateKey privateKey,
+                               IPublicKey[] publicKeys) : this(new KeyknoxManager(accessTokenProvider, privateKey, publicKeys))
+        {
+        }
+        private async Task<Dictionary<string, CloudEntry>> RetrieveCloudEntries(){
+            var decryptedKeyknoxValue = await keyknoxManager.PullValueAsync();
+            var entries = serializer.Deserialize<List<CloudEntry>>(Bytes.ToString(decryptedKeyknoxValue.Value));
+            var namedEntries = new Dictionary<string, CloudEntry>();
+            syncWasCalled = true;
+            this.keyknoxHash = decryptedKeyknoxValue.KeyknoxHash;
+            entries.ForEach(entry => namedEntries.Add(entry.Name, entry));
+            cloudEntries = namedEntries;
+            return namedEntries;
+           // cacheEntries(deserializeEntries(response.value), true)
+           // this.decryptedKeyknoxData = response
+        }
+
+    }
+}
---
+++ Keyknox/IKeyStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Virgil.SDK;
+
+namespace Keyknox
+{
+    public interface IKeyStorage
+    {
+        CloudEntry Store(string name, byte[] data, Dictionary<string, string> meta);
+        void UpdateEntry(string name, byte[] data, Dictionary<string, string> meta);
+        CloudEntry RetrieveEntry(string name);
+        CloudEntry[] RetrieveAllEntries();
+        void DeteleEntry(string name);
+        bool ExistsEntry(string name);
+    }
+}
