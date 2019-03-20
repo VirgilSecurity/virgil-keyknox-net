99ce6581bf6c128b0d95f2f97b8af49a83146245
Vasilina <marfachaiko@gmail.com>
2019-03-20 16:46:28 +0500

    Cloud key storage entry

---
+++ Keyknox/CloudEntry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Keyknox
+{
+    [DataContract]
+    public class CloudEntry
+    {
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "data")]
+        public byte[] Data { get; set; }
+
+        [DataMember(Name = "creation_date")]
+        public DateTime CreationDate { get; set; }
+
+        [DataMember(Name = "modification_date")]
+        public DateTime ModificationDate { get; set; }
+
+        [DataMember(Name = "meta", EmitDefaultValue = false)]
+        public Dictionary<string, string> Meta { get; set; }
+    }
+}
